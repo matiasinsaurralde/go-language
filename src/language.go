83f980fa54d3f2d9d4d675ae079adf8b6f728876
Matias Insaurralde <matiasbaruchinsaurralde@gmail.com>
2013-04-30 02:17:03 -0400

    orderPlease()

--- src/language.go
+++ src/language.go
@@ -1,20 +1,84 @@
 package language
 
-import ( "fmt"
-	 "strings" )
+import(	"strings"
+	"io/ioutil"
+	"sort"
+	"fmt" )
 
-func LoadText( fileName string ) string {
-
-	fmt.Printf( "loading file '%s'.\n", fileName )
-
-	return "this is a sample sentence"
+func ReadText( fileName string ) string {
+	fileContent, _ := ioutil.ReadFile( fileName )
+	return string( fileContent )
 }
 
-func ExtractLetters( text string ) {
+func ExtractLetters( text string ) map[string]int {
 
-	splits := strings.Split( text, "" )
+	letters := make( map[string]int )
 
-	fmt.Printf( "original string: '%s'\n", text )
+	splits := strings.Split( strings.ToLower( text ), "" )
 
-	fmt.Printf( "splits: %q\n", splits )
+	for i := 0; i < len( splits ); i++ {
+		letters[ splits[ i ] ] += 1
+	}
+
+	/* 
+
+		var k string
+		var v interface {}
+
+		for k, v = range letters {
+			fmt.Printf( "%s => %d\n", k, v )
+		}
+
+	*/
+
+	return letters
+
+}
+
+func BuildModel( corpusFilename string, modelName string  ) {
+
+	corpus := ReadText( corpusFilename )
+
+	output := ExtractLetters( corpus )
+	ordered := OrderPlease( output )
+
+	var k string
+	var v interface {}
+
+	for k, v = range ordered {
+		 fmt.Printf( "%s => %d\n", k, v )
+	}
+
+	// fmt.Printf("%s\n", corpus)
+
+}
+
+func OrderPlease( letters map[string]int ) map[string]int {
+
+	var keys []int
+	sw := make( map[int]string )
+	fm := make ( map[string]int )
+
+	for k, v := range letters {
+		sw[ v ] = k
+	}
+
+	for k := range sw {
+		keys = append( keys, k )
+	}
+
+	sort.Ints( keys )
+
+	for _, k := range keys {
+		// fmt.Printf("key: %d, Value: %s", k, sw[ k ] )
+		fm[ sw[k] ] = 0
+	}
+
+	for k,v := range fm {
+		fmt.Printf("%s => %d\n", k, v )
+	}
+
+	fmt.Printf("***\n")
+
+	return letters
 }
